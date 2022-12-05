89729d44b1253343da9aceb8bd9053a04b15a242
madhof <charlesJD@outlook.fr>
2022-12-05 15:20:26 +0100

    test de l'intégration d'un input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@ import MyButton from "./../components/MyButton.vue";
 import MyTitle from "./../components/MyTitle.vue";
 import MyHeader from "./../components/MyHeader.vue";
 import MyText from "./../components/MyText.vue";
+import MyInput from "./../components/MyInput.vue";
+
 export default {
   components: {
     MyButton,
