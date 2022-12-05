8636cf3d05bfef1019cae4792405dca94f67337f
meline <melina.terrier@gmail.com>
2022-12-05 15:16:57 +0100

    Ajout header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <script>
 import MyButton from "./../components/MyButton.vue";
 import MyTitle from "./../components/MyTitle.vue";
+import MyHeader from "./../components/MyHeader.vue";
 export default {
   components: {
     MyButton,
     MyTitle,
+    MyHeader,
   },
 };
 </script>
 
 <template>
   <main>
+    <MyHeader />
     <MyTitle />
     <MyTitle size="big" label="grand titre h1" />
     <MyTitle size="tiny" type="h2" label="petit titre h2" />
