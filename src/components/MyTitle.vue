3bcaa84d1f72111a8804246a6d290d62e5f3c2f7
meline <melina.terrier@gmail.com>
2022-12-05 13:56:57 +0100

    Passage au vue

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,32 @@
+<script>
+export default{
+  data() {
+    return {
+      className: `title ${this.size}`,
+    };
+  },
+  props: {
+    size: String,
+    type: String,
+    label: String,
+  },
+};
+</script>
+
+<template>
+  <h1 :class="className">{{ label }}</h1>
+</template>
+
+<style lang="scss" scoped>
+@import "./../scss/foundations/variables";
+.title {
+  font-family: $textFont;
+  font-size: 2rem;
+  &.-tiny {
+    font-size: 1rem;
+  }
+  &.-big {
+    font-size: 3rem;
+  }
+}
+</style>
