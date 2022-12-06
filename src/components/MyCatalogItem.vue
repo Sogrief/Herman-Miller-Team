5b95b6d33cd9da8f6a78fe8147ecad6335f1d64f
madhof <charlesJD@outlook.fr>
2022-12-06 09:18:58 +0100

    création MyCatalogItem.vue

---
+++ src/components/MyCatalogItem.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default{
+    props: 
+    {
+        src: String,
+    },
+};
+</script>
+
+<template>
+
+    <div class="catalog__item">
+        <img :src="src" />
+    </div>
+
+</template>
+
+<style>
+
+</style>
