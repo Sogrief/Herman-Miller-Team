f71d70fa44f81cda7c5f1113ef9fcf8bf0df65d3
madhof <charlesJD@outlook.fr>
2022-12-06 09:59:35 +0100

    mise à jour catalogItem.vue

--- src/components/MyCatalogItem.vue
+++ src/components/MyCatalogItem.vue
@@ -1,33 +1,57 @@
 <script>
 
 export default{
-    props: 
+    components:
     {
-        src: String,
-        nomItem:String,
+        catalogCard,
     },
+
+    data() {
+    return {
+      produits: [
+        {
+          id: 1,
+          srcImage: "../../assets/images/produit 1.png",
+          nomItem: "Embody",
+          prix:"1730€"
+        },
+        {
+          id: 2,
+          srcImage: "../../assets/images/produit 1.png",
+          nomItem: "Aeron",
+          prix:"1725€"
+        },
+        {
+          id: 3,
+          srcImage: "../../assets/images/produit 1.png",
+          nomItem: "Vantum",
+          prix:"1277€",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
 
-    <div class="catalog__item">
-        <img :src="src" />
-        <div class="catalog__item__name">{{nomItem}}</div>
+    <div class="catalog">
+        <catalogCard
+            v-for="produit in produits"
+            :key="produit.id"
+            :srcImage="produit.srcImage"
+            :nomItem="produit.nomItem"
+            :prix="produit.prix"
+        />
     </div>
 
 </template>
 
 <style lang="scss" scoped>
 
-    .catalog__item
+    .catalog
     {
         display:flex;
-
-        &__name
-        {
-            @include title;
-        }
     }
 
 </style>
