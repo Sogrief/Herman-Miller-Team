b4775da936e7813966ce92397420437f3c95f0cd
meline <melina.terrier@gmail.com>
2022-12-06 09:56:12 +0100

    ajout boutons et checkbox

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -2,36 +2,20 @@
 export default {
   data() {
     return {
-      className: `button ${this.type === "secondary" ? "-secondary" : ""}`,
+      className: `button -${this.type}`,
     };
   },
   props: {
     label: String,
     type: String,
-    href: String,
   },
 };
 </script>
 
 <template>
-  <a v-if="href" :href="href" :class="className">
-    {{ label }}
-  </a>
-  <button v-else :class="className">
+  <button :class="className">
     {{ label }}
   </button>
-
-  <a v-if="href" :href="href" :class="colorCheck">
-    {{ label }}
-  </a>
-  <button v-else :class="colorCheck">
-    {{ label }}
-  </button>
-
-  <div class="colorCheck">
-    <input type="checkbox" id="checkbox" />
-    <label for="checkbox"></label>
-  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -46,108 +30,30 @@ export default {
   &:hover {
     color: white;
   }
-
   //boutton avec flèche
   &.-suivant {
     &::after {
-      border-style: solid;
-      border-width: 0.12em 0.12em 0 0;
-      content: "";
-      display: inline-block;
-      height: pxToRem(6);
-      left: pxToRem(3);
-      position: relative;
+      @include button();
       transform: rotate(45deg);
-      vertical-align: center;
-      width: pxToRem(6);
     }
   }
 
   &.-precedent {
     &::before {
-      border-style: solid;
-      border-width: 0.12em 0.12em 0 0;
-      content: "";
-      display: inline-block;
-      height: pxToRem(6);
-      right: pxToRem(3);
-      position: relative;
+      @include button();
       transform: rotate(-135deg);
-      vertical-align: center;
-      width: pxToRem(6);
     }
   }
-}
-
-button.quantité {
-  width: 20px;
-  height: 20px;
-  background: transparent;
-  color: $mainColor;
-  border: 1px solid $mainColor;
-
-  &:hover {
-    background: $mainColor;
-    color: #2b2b2b;
-    border: none;
-  }
-}
-
-//colorCheck
-.colorCheck {
-  input[type="checkbox"] {
-    display: none;
-
-    &:checked + label {
-      background-color: transparent;
-    }
-
-    &:checked + label:before {
-      opacity: 1;
-    }
-    &:checked + label:after {
-      opacity: 1;
-    }
-  }
-
-  label {
-    background-color: $bodyText;
-    cursor: pointer;
-    height: 20px;
+  &.-quantite {
     width: 20px;
-    position: absolute;
-    display: flex;
-    &:before {
-      height: 15px;
-      width: 15px;
-      opacity: 0;
-      content: "";
-      position: absolute;
-      top: 4.5px;
-      left: 4.5px;
-      background-color: $bodyText;
-    }
-    &:after {
-      border: 2px solid $mainColor;
-      width: 20px;
-      height: 20px;
-      position: absolute;
-      content: "";
-      opacity: 0;
-      border-style: dashed;
-    }
-  }
-  .button {
-    padding: 1rem;
-    display: inline-block;
-    background-color: black;
-    color: white;
-    text-decoration: none;
-    border: 1px solid;
-    border-radius: 5px;
+    height: 20px;
+    background: transparent;
+    color: $mainColor;
+    border: 1px solid $mainColor;
     &:hover {
-      background-color: black;
-      color: white;
+      background: $mainColor;
+      color: #2b2b2b;
+      border: none;
     }
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,28 +3,28 @@ import MyButton from "./../components/MyButton.vue";
 import MyTitle from "./../components/MyTitle.vue";
 import MyHeader from "./../components/MyHeader.vue";
 import MyText from "./../components/MyText.vue";
+import MyCheckbox from "./../components/MyCheckbox.vue";
 export default {
   components: {
     MyButton,
     MyTitle,
     MyHeader,
     MyText,
+    MyCheckbox,
   },
 };
 </script>
 
 <template>
   <main>
-
     <MyTitle />
     <MyTitle size="big" label="grand titre h1" />
     <MyTitle size="tiny" type="h2" label="petit titre h2" />
-    <MyButton label="Cliquez ici" href="/lien" />
-
+    <MyButton type="precedent" label="suivant" />
     <MyHeader />
+    <MyCheckbox />
     <MyTitle size="-little" label="test" />
     <MyText size="-body" label="blabla" />
     <MyText size="-liens" label="blabla" />
-   
   </main>
 </template>
