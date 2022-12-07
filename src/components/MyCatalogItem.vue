16496a285a8ee9e07d8983266de9c62f32195cb4
madhof <charlesJD@outlook.fr>
2022-12-07 10:53:28 +0100

    effet de lumière en fond des images

--- src/components/MyCatalogItem.vue
+++ src/components/MyCatalogItem.vue
@@ -39,6 +39,7 @@ export default {
     img
     {
       //box-shadow: 0px 0px 179px -81px rgba(155, 155, 155, 0.71);
+      filter:drop-shadow(0px 0px 100px white);
       height:pxToRem(300);
     }
     
