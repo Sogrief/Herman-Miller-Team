c9dbbb4b690a7ddc2d41a2c9f1aa001caff395d3
Léa <lezervinia@gmail.com>
2023-01-27 22:24:06 +0100

    Ajout de conseils + test de nouvelles requete de page

---
+++ src/views/AdviceView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="conseils">
+    {{page.title.rendered}}
+  </div>
+  <div v-if="page.content" v-html="page.content.rendered"></div>
+
+</template>
+
+<script>
+    import { client } from "@/outils/axios"
+export default {
+    data() {
+        return{
+            page:{}
+        };
+    },
+
+    async created () {
+    const response = await client.get(
+      import.meta.env.VITE_WP_API_URL + "/wp/v2/pages?slug=conseils&_fields=id,title,content,date,excerpt"
+    );
+     this.page = response.data[0];
+     
+   },
+
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
