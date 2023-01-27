cbd90310f61e12716a4447e8b422f308f1105754
Léa <lezervinia@gmail.com>
2023-01-27 22:24:51 +0100

    Création du router pour la page conseils

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
+      path: '/conseils',
+      name: 'conseils',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AdviceView.vue')
+    },
+    {
       path: '/products',
       name: 'products',
       // route level code-splitting
