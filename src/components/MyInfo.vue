4f3bab50ff845b06bb3b4d54d45b6df6b4d49107
Léa <lezervinia@gmail.com>
2023-01-29 19:25:10 +0100

    Test ACF

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -1,32 +1,76 @@
 <script>
 import MyTitle from "./MyTitle.vue";
 export default {
-  props: {
-    title: String,
-    srcImage: String,
-    label: String,
-  },
   components: {
     MyTitle,
   },
-};
+  props: {
+    acf: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+
 </script>
+
 <template>
-  <div class=" infoCard">
-    <div class="infoCard__image">
-      <img :src="srcImage" :alt="title" />
+  <div class=" infoCard" v-if="acf.infos">
+     <div class="infoCard__image"
+     v-for="item in acf.infos"
+     :key="item.id">
+      <img :src="item.img.url" :alt="title" />
+      <MyTitle type="h3" class="infoCard__title" label="{{ item.infos.title }}" />
     </div>
-    <MyTitle
-      class="infoCard__title"
-      size="-default"
-      type="h2"
-      v-bind:label="title"
-    />
-    <p class="infoCard__text">{{ label }}</p>
+    <p class="infoCard__text">{{ item.infos.text }}</p>
   </div>
 </template>
 
-<style lang="scss" scoped>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default {
+//   props: {
+//     title: String,
+//     srcImage: String,
+//     label: String,
+//   },
+//   components: {
+//     MyTitle,
+//   },
+// };
+// 
+<!-- // <template>
+//   <div class=" infoCard">
+//     <div class="infoCard__image">
+//       <img :src="srcImage" :alt="title" />
+//     </div>
+//     <MyTitle
+//       class="infoCard__title"
+//       size="-default"
+//       type="h2"
+//       v-bind:label="title"
+//     />
+//     <p class="infoCard__text">{{ label }}</p>
+//   </div>
+// </template>
+
+/* <style lang="scss" scoped>
 .infoCard {
   @include bodyText();
   &__title {
@@ -44,4 +88,6 @@ export default {
     max-width: 220px;
   }
 }
-</style>
+</style>  -->
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import MyButton from "@/components/MyButton.vue";
 import MyTitle from "@/components/MyTitle.vue";
 import MyHeader from "@/components/MyHeader.vue";
 import MyText from "@/components/MyText.vue";
-import MyInfoCard from "@/components/MyInfoCard.vue";
+import MyInfo from "@/components/MyInfo.vue";
 import MyCheckbox from "@/components/MyCheckbox.vue";
 import MyFooter from "@/components/MyFooter.vue";
 import DefaultLayout from "@/layout/DefaultLayout.vue";
@@ -15,10 +15,23 @@ export default {
     MyTitle,
     MyHeader,
     MyText,
-    MyInfoCard,
+    MyInfo,
     MyCheckbox,
     MyFooter,
     DefaultLayout,
+  },
+
+  data(){
+    return {
+      infos: {},
+    }
+  },
+  async mounted() {
+    const response = await client.get(
+      import.meta.env.VITE_WP_API_URL +
+        "/wp/v2/pages/11"
+    );
+    this.infos = response.data[0];
   },
 };
 </script>
@@ -42,7 +55,7 @@ export default {
         <main>
           <MyTitle size="-enormous" label="Aeron" type="h1" />
           <MyButton label="Découvrir" />
-          <MyInfoCard />
+          <MyInfo />
         </main>
         <template v-slot:footer>
           <MyFooter />
