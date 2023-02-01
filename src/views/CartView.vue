27b0d36edc61913c54c5cb8ca9d408e383fca81c
Charles <charlesJD@outlook.fr>
2023-02-01 22:45:05 +0100

    panier affichage produits
    récupération des produits du panier et leurs informations(image, prix, etc...)

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -2,7 +2,17 @@
 
     <MyHeader />
 
-    {{ $store.state.products }}
+
+    {{ $store.state.products.name}}
+
+    <div class="panier" v-for="addedProducts in $store.state.products">
+        <div>
+            <div>{{addedProducts.name}}</div>
+            <div>{{addedProducts.price}}</div>
+            <div>{{addedProducts.quantity}}</div>
+            <img v-bind:src="addedProducts.images[0].src">
+        </div>
+    </div>
 
 </template>
 
