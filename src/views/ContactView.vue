981592d0cdd09624c40380efd6ce042fd74de2fb
Léa <lezervinia@gmail.com>
2023-02-02 20:42:36 +0100

    Création de la page contact

---
+++ src/views/ContactView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="contact">
+      {{ page.title.rendered }}
+    </div>
+    <div v-if="page.content" v-html="page.content.rendered"></div>
+  </template>
+
+<script>
+import { client } from "@/outils/axios";
+export default {
+  data() {
+    return {
+      page: {},
+    };
+  },
+
+  async created() {
+    const response = await client.get(
+      import.meta.env.VITE_WP_API_URL +
+        "/wp/v2/pages?slug=contact&_fields=id,title,content,date,excerpt"
+    );
+    this.page = response.data[0];
+  },
+};
+</script>
+
+<style>
+</style>
